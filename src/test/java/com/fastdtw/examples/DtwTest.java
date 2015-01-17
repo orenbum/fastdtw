ddd75707bc4cb3b8ed34bb6c711c7e74fced461e
Dave Moten <davidmoten@gmail.com>
2015-01-17 22:59:33 +1100

    remove DTW methods that are not as good as FastDTW, make standard DtwTest a real junit test

--- src/test/java/com/fastdtw/examples/DtwTest.java
+++ src/test/java/com/fastdtw/examples/DtwTest.java
@@ -7,44 +7,41 @@
 
 package com.fastdtw.examples;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import com.fastdtw.dtw.TimeWarpInfo;
+import com.fastdtw.dtw.WarpPath;
 import com.fastdtw.timeseries.TimeSeries;
-import com.fastdtw.util.DistanceFunction;
-import com.fastdtw.util.DistanceFunctionFactory;
+import com.fastdtw.util.EuclideanDistance;
 
+public class DtwTest {
 
-public class DtwTest
-{
+    private static final double PRECISION = 0.00000001;
 
-   // PUBLIC FUNCTIONS
-   public static void main(String[] args)
-   {
-      if (args.length!=2 && args.length!=3)
-      {
-         System.out.println("USAGE:  java DtwTest timeSeries1 timeSeries2 [EuclideanDistance|ManhattanDistance|BinaryDistance]");
-         System.exit(1);
-      }
-      else
-      {
-         final TimeSeries tsI = new TimeSeries(args[0], false, false, ',');
-         final TimeSeries tsJ = new TimeSeries(args[1], false, false, ',');
-         
-         final DistanceFunction distFn;
-         if (args.length < 3)
-         {
-            distFn = DistanceFunctionFactory.getDistFnByName("EuclideanDistance"); 
-         }
-         else
-         {
-            distFn = DistanceFunctionFactory.getDistFnByName(args[2]);
-         }   // end if
-         
-         final TimeWarpInfo info = com.fastdtw.dtw.DTW.getWarpInfoBetween(tsI, tsJ, distFn);
+    @Test
+    public void testDistanceBetweenTrace0AndTrace1() {
+        final TimeSeries tsI = new TimeSeries("src/test/resources/trace0.csv", false, false, ',');
+        final TimeSeries tsJ = new TimeSeries("src/test/resources/trace1.csv", false, false, ',');
 
-         System.out.println("Warp Distance: " + info.getDistance());
-         System.out.println("Warp Path:     " + info.getPath());
-      }  // end if
+        final TimeWarpInfo info = com.fastdtw.dtw.DTW.getWarpInfoBetween(tsI, tsJ,
+                new EuclideanDistance());
+        assertEquals(9.139400704860002, info.getDistance(), PRECISION);
+        // [(0,0),(0,1),(1,2),(2,3),...,(272,272),(273,272),(274,273),(274,274)]
+        WarpPath p = info.getPath();
+        assertEquals(0, p.get(0).getCol());
+        assertEquals(0, p.get(0).getRow());
+        assertEquals(0, p.get(1).getCol());
+        assertEquals(1, p.get(1).getRow());
 
-   }  // end main()
+        assertEquals(1, p.get(2).getCol());
+        assertEquals(2, p.get(2).getRow());
 
-}  // end class DtwTest
+        assertEquals(274, p.get(p.size() - 2).getCol());
+        assertEquals(273, p.get(p.size() - 2).getRow());
+        assertEquals(274, p.get(p.size() - 1).getCol());
+        assertEquals(274, p.get(p.size() - 1).getRow());
+    }
+
+}
