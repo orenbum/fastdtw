7d9df9127fe492692b88cf4f22f01d20827eec9b
Dave Moten <davidmoten@gmail.com>
2015-01-21 13:57:22 +1100

    whitespace

--- src/main/java/com/fastdtw/matrix/ColMajorCell.java
+++ src/main/java/com/fastdtw/matrix/ColMajorCell.java
@@ -7,50 +7,37 @@
 
 package com.fastdtw.matrix;
 
+public class ColMajorCell {
+    private final int col;
+    private final int row;
 
-public class ColMajorCell
-{
-   private final int col;
-   private final int row;
+    public int getCol() {
+        return col;
+    }
 
+    public int getRow() {
+        return row;
+    }
 
-   public int getCol()
-   {
-      return col;
-   }
+    public ColMajorCell(int column, int row) {
+        this.col = column;
+        this.row = row;
+    } // end Constructor
 
+    @Override
+    public boolean equals(Object o) {
+        return (o instanceof ColMajorCell) && (((ColMajorCell) o).col == this.col)
+                && (((ColMajorCell) o).row == this.row);
+    }
 
+    @Override
+    public int hashCode() {
+        return (1 << col) + row;
+    }
 
-   public int getRow()
-   {
-     return row;
-   }
+    @Override
+    public String toString() {
+        return "(" + col + "," + row + ")";
+    }
 
-
-   public ColMajorCell(int column, int row)
-   {
-      this.col = column;
-      this.row = row;
-   }  // end Constructor
-
-
-   public boolean equals(Object o)
-   {
-      return (o instanceof ColMajorCell) &&
-             (((ColMajorCell)o).col == this.col) &&
-             (((ColMajorCell)o).row == this.row);
-   }
-
-
-   public int hashCode()
-   {
-      return (1<<col) + row;
-   }
-
-
-   public String toString()
-   {
-      return "(" + col + "," + row + ")";
-   }
-
-}  // end class ColMajorCell
+} 
